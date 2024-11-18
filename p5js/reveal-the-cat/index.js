7071d100095ecbac5a1db1a570687c10e93e0cc1
ff6347 <hash@ff6347.email>
2024-11-18 20:28:27 +0100

    fix: reveal-the-cat black bg

--- p5js/reveal-the-cat/index.js
+++ p5js/reveal-the-cat/index.js
@@ -6,16 +6,17 @@ function preload() {
 function setup() {
 	createCanvas(300, 200);
 	rectMode(CENTER);
-	// imageMode(CENTER)
+	stroke('white');
+	fill('black');
 }
 
 function draw() {
 	image(img, 0, 0);
 	// Get a region of the image.
 	let c = get(mouseX - 15, mouseY - 15, 15, 15);
-	noStroke;
-	fill('black');
+
+	// mask the image
 	rect(width / 2, height / 2, width, height);
-	// Display the region.
+	// Display only the region.
 	image(c, mouseX - 15, mouseY - 15);
 }
