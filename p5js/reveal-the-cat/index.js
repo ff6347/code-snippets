3b5c767f1a68168925c14622631491928c0449ee
ff6347 <hash@ff6347.email>
2024-11-18 20:25:53 +0100

    feat: Reveal the cat

---
+++ p5js/reveal-the-cat/index.js
@@ -0,0 +1,21 @@
+let img;
+
+function preload() {
+	img = loadImage('data/kitten-300-200.jpg');
+}
+function setup() {
+	createCanvas(300, 200);
+	rectMode(CENTER);
+	// imageMode(CENTER)
+}
+
+function draw() {
+	image(img, 0, 0);
+	// Get a region of the image.
+	let c = get(mouseX - 15, mouseY - 15, 15, 15);
+	noStroke;
+	fill('black');
+	rect(width / 2, height / 2, width, height);
+	// Display the region.
+	image(c, mouseX - 15, mouseY - 15);
+}
