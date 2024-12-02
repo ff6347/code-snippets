c4179298a07305544e7c05a5b3cfedfc0ea010a0
ff6347 <hash@ff6347.email>
2024-12-02 11:22:06 +0100

    feat!: Move motion sketches to own folder
    BREAKING CHANGES: This will break some links

---
+++ p5js/motion/accelerate/index.js
@@ -0,0 +1,23 @@
+/**
+ * Why separate the acceleration from the x value?
+ * One problem that might occur when we don't do that is that x can't be negative.
+ */
+
+let radius = 10;
+let x = -radius;
+let accel = 1;
+function setup() {
+	const canvas = createCanvas(100, 100);
+	canvas.parent('sketch');
+}
+
+function draw() {
+	background(0);
+	accel *= 1.05;
+	x += accel;
+	ellipse(x, 50, radius, radius);
+	if (x > width + radius) {
+		x = -radius;
+		accel = 1;
+	}
+}
