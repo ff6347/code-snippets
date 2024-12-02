c13b89dfbc0beb09fb9242a934211be6c2b716d6
ff6347 <hash@ff6347.email>
2024-12-02 12:22:54 +0100

    feat: Update ease in out with more equations

--- p5js/motion/ease-out/index.js
+++ p5js/motion/ease-out/index.js
@@ -16,27 +16,27 @@ function draw() {
 	x = (1 - t) * width;
 
 	// Quadratic
-	x = (1 - pow(t, 2)) * width;
+	x = (1 - pow(1 - t, 2)) * width;
 
 	// Cubic
-	x = (1 - pow(t, 3)) * width;
+	x = (1 - pow(1 - t, 3)) * width;
 
 	// Quartic
-	x = (1 - pow(t, 4)) * width;
+	x = (1 - pow(1 - t, 4)) * width;
 
 	// Quintic
-	x = (1 - pow(t, 5)) * width;
+	x = (1 - pow(1 - t, 5)) * width;
 
 	// Sine based
-	x = sin((t * PI) / 2) * width;
+	x = cos(((1 - t) * PI) / 2) * width;
 
 	// Exponential
 	x = (1 - pow(2, -10 * t)) * width;
 
 	circle(x, height / 2, 20);
 
-	if (x <= 0) {
+	if (x >= width) {
 		t = 0;
-		x = width;
+		x = 0;
 	}
 }
